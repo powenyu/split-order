9f7a8a2b37e5ad411274720f79c62e400eb9da68
Powei Yu <powei.yu@ucfunnel.com>
2022-08-31 19:24:06 +0800

    first commit that can return pong when receive pong

---
+++ bot/bot.go
@@ -0,0 +1,53 @@
+package bot
+
+import (
+	"fmt" //to print errors
+
+	"github.com/bwmarrin/discordgo"         //discordgo package from the repo of bwmarrin .
+	"github.com/powenyu/split-order/config" //importing our config package which we have created above
+)
+
+var BotId string
+var goBot *discordgo.Session
+
+func Start() {
+
+	//creating new bot session
+	goBot, err := discordgo.New("Bot " + config.Token)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	// Making our bot a user using User function .
+	u, err := goBot.User("@me")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	// Storing our id from u to BotId .
+	BotId = u.ID
+
+	// Adding handler function to handle our messages using AddHandler from discordgo package. We will declare messageHandler function later.
+	goBot.AddHandler(messageHandler)
+
+	err = goBot.Open()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println("Bot is running !")
+}
+
+//Definition of messageHandler function it takes two arguments first one is discordgo.Session which is s , second one is discordgo.MessageCreate which is m.
+func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+	//Bot musn't reply to it's own messages , to confirm it we perform this check.
+	if m.Author.ID == BotId {
+		return
+	}
+	//If we message ping to our bot in our discord it will return us pong .
+	if m.Content == "ping" {
+		_, _ = s.ChannelMessageSend(m.ChannelID, "pong")
+	}
+}
---
+++ config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"       //used to print errors majorly.
+	"io/ioutil" //it will be used to help us read our config.json file.
+)
+
+var (
+	Token     string //To store value of Token from config.json .
+	BotPrefix string // To store value of BotPrefix from config.json.
+
+	config *configStruct //To store value extracted from config.json.
+)
+
+type configStruct struct {
+	Token     string `json : "Token"`
+	BotPrefix string `json : "BotPrefix"`
+}
+
+func ReadConfig() error {
+	fmt.Println("Reading config file...")
+	file, err := ioutil.ReadFile("./config.json")
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	fmt.Println(string(file))
+	err = json.Unmarshal(file, &config)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	Token = config.Token
+	BotPrefix = config.BotPrefix
+
+	return nil
+}
